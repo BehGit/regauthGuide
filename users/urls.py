e673a98f1450cf4e167d77599f228eae888fc861
Beh <bb@gmail.com>
2025-07-08 22:20:26 +0500

    new push

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
-from .views import RegisterView
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+#from django.urls import path
+#from .views import RegisterView, LoginView
+#from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-urlpatterns = [
-    path('register/', RegisterView.as_view(), name='register'),
-    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-]
+#urlpatterns = [
+   # path('register/', RegisterView.as_view(), name='register'),
+   # path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+   # path("login/", LoginView.as_view(), name="rest_login"),
+   # path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+#]
